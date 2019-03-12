0f0ad650621791a00b14903f6de3e76a7d668071
piojo2287 <33067911+piojo2287@users.noreply.github.com>
2019-03-12 09:38:37 -0300

    proyecto funcionando - con falla en constructor

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -8,7 +8,7 @@
         <div class="col-md-5">
           <div class="card">
             <div class="card-body">
-              <form @submit.prevent="addPaciente">
+              <form @submit.prevent="sendPaciente">
                 <div class="form-group">
                   <input
                     type="text"
@@ -53,11 +53,42 @@
                     class="form-control"
                   >
                 </div>
-
-                <button class="btn btn-primary btn-block">Enviar</button>
+                <template v-if="edit === false">
+                  <button class="btn btn-primary btn-block">Guardar</button>
+                </template>
+                <template v-else>
+                  <button class="btn btn-primary btn-block">Actualizar</button>
+                </template>
               </form>
             </div>
           </div>
+        </div>
+
+        <div class="col-md-7">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>fecha Nacimiento</th>
+                <th>Genero</th>
+                <th>Domicilio</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="paciente of pacientes" v-bind:key="paciente">
+                <td>{{paciente.nombre}}</td>
+                <td>{{paciente.apellido}}</td>
+                <td>{{paciente.fecha_nac}}</td>
+                <td>{{paciente.genero}}</td>
+                <td>{{paciente.domicilio}}</td>
+                <td>
+                  <button @click="deletePaciente(paciente._id)" class="btn btn-danger">X</button>
+                  <button @click="editPaciente(paciente._id)" class="btn btn-secondary">Edit</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
@@ -85,18 +116,88 @@ export default {
         fecha_nac: "",
         genero: "",
         domicilio: ""
-      }
-    }
+      },
+      pacientes: [],
+      edit: false,
+      pacienteToEdit: ""
+    };
   },
+  created() {
+    this.getPaciente();
+  },
+
   methods: {
-    addPaciente() {
-      console.log(this.paciente)
-  
+    sendPaciente() {
+      if (this.edit === false) {
+        fetch("/api/pacientes", {
+          method: "POST",
+          body: JSON.stringify(this.paciente),
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            this.getPaciente();
+          });
+      } else {
+        fetch("/api/pacientes/" + this.pacienteToEdit, {
+          method: "PUT",
+          body: JSON.stringify(this.paciente),
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            this.getPaciente();
+            this.edit = false;
+          });
+      }
+
       this.paciente.nombre = "";
       this.paciente.apellido = "";
       this.paciente.fecha_nac = "";
       this.paciente.genero = "";
       this.paciente.domicilio = "";
+    },
+    getPaciente() {
+      fetch("/api/pacientes")
+        .then(res => res.json())
+        .then(data => {
+          this.pacientes = data;
+          console.log(this.pacientes);
+        });
+    },
+    deletePaciente(id) {
+      fetch("api/pacientes/" + id, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json"
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          this.getPaciente();
+        });
+    },
+    editPaciente(id) {
+      fetch("api/pacientes/" + id)
+        .then(res => res.json())
+        .then(data => {
+          this.paciente = {
+            nombre: data.nombre,
+            apellido: data.apellido,
+            fecha_nac: data.fecha_nac,
+            genero: data.genero,
+            domicilio: data.domicilio
+          };
+          this.pacienteToEdit = data._id;
+          this.edit = true;
+        });
     }
   }
 };
